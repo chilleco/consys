33dfa9319186e6bdbafa19e096f47f421addd09d
Алексей Полоз <polozhev@mail.ru>
2021-09-26 01:19:28 +0300

    Pilot

---
+++ README.md
@@ -0,0 +1,4 @@
+# ConSys
+Object-oriented programming management system in Python
+
+[PyPI](https://pypi.org/project/consys/)
---
+++ consys/__init__.py
@@ -0,0 +1,14 @@
+"""
+Initializing the Python package
+"""
+
+from .main import Base, Attribute
+
+
+__version__ = '0.1'
+
+__all__ = (
+    '__version__',
+    'Base',
+    'Attribute',
+)
